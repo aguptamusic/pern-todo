a4633326d1386e36fa6f36e80e74e19a44d45435
aguptamusic <aguptamusic@gmail.com>
2020-12-20 13:09:41 -0800

    add edit functionality

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,5 +1,7 @@
 import React, { Fragment, useEffect, useState } from "react";
 
+import EditTodo from "./EditTodo";
+
 const ListTodos = () => {
   const [todos, setTodos] = useState([]); //create todos state
 
@@ -58,7 +60,9 @@ const ListTodos = () => {
           {todos.map((todo) => (
             <tr key={todo.todo_id}>
               <td>{todo.description}</td>
-              <td>Edit</td>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
               <td>
                 <button
                   className="btn btn-danger"
